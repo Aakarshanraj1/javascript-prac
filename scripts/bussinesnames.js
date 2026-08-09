2a9455cfe571dbe5fe24f5801b0343054e0d9591
Aakarshan raj <aksh2802.mr@gmail.com>
2026-08-09 22:05:50 +0530

    add random business name generator

---
+++ scripts/bussinesnames.js
@@ -0,0 +1,10 @@
+let ad = ["Crazy", "Amazing", "Fire"];
+let sn = ["Engine", "Foods", "Garments"];
+let aw = ["Bros", "Limited", "Hubs"];
+
+
+let b= Math.floor(Math.random()*(2-0+1))+0;
+let a= Math.floor(Math.random()*(2-0+1))+0;
+let c= Math.floor(Math.random()*(2-0+1))+0;
+console.log(b ,a ,c)
+console.log(`${ad[b]} ${sn[a]} ${aw[c]}`);
