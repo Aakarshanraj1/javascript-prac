5a20de6b7a9c0e0d1fae9082bc3ec457124b6099
Aakarshan raj <aksh2802.mr@gmail.com>
2026-08-09 09:04:40 +0530

    added a faulty calculator

---
+++ scripts/faultycal.js
@@ -0,0 +1,47 @@
+
+const a = Math.random();
+if (a < 0.1) {
+
+
+
+    function sum(a, b) {
+     
+        console.log(a - b);
+    }
+    function subtract(a, b) {
+        
+        console.log(a / b);
+    }
+    function multiply(a, b) {
+       
+        console.log(a + b);
+    }
+    function divide(a, b) {
+      
+        console.log(a ** b);
+    }
+
+
+
+}
+else {
+
+    function sum(a, b) {
+       
+        console.log(a + b);
+    }
+    function subtract(a, b) {
+        
+        console.log(a - b);
+    }
+    function multiply(a, b) {
+       
+        console.log(a * b);
+    }
+    function divide(a, b) {
+       
+        console.log(a / b);
+    }
+
+}
+sum(2,2);
